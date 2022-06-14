9a72433c512dc70177b96808a3eac88a57e2db99
minsson <mskaang@gmail.com>
2022-06-15 00:22:20 +0900

    테이블뷰연습: 모델로 사용할 Emoji 구조체 구현

---
+++ TableViewPractice/TableViewPractice/Models/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  TableViewPractice
+//
+//  Created by minsson on 2022/06/15.
+//
+
+import Foundation
+import UIKit
+
+struct Emoji {
+    let image: UIImage
+    let name: String
+    let description: String
+}
