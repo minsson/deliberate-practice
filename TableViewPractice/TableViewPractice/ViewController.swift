ceb1ff06e60bc0ee393e4226c0039545e85cbbdd
minsson <mskaang@gmail.com>
2022-06-14 23:10:33 +0900

    테이블뷰연습: 프로젝트 생성

---
+++ TableViewPractice/TableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by minsson on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
