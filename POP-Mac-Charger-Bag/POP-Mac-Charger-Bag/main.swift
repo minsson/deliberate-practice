696a5bab6444dc7ca021c07796669b6b2c004e16
minsson <mskaang@gmail.com>
2022-07-02 10:24:40 +0900

    POP연습01: 기능을 위해 필요한 Chargeable Protocol 설계

--- POP-Mac-Charger-Bag/POP-Mac-Charger-Bag/main.swift
+++ POP-Mac-Charger-Bag/POP-Mac-Charger-Bag/main.swift
@@ -5,3 +5,12 @@
 //  Created by minsson on 2022/07/02.
 //
 
+protocol Chargeable {
+    var maximumWattPerHour: WattPerHour { get }
+    
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
+    // 이 메서드는 충전기와 기기의 허용 와트시를 검사해서, 기기의 허용 와트시가 더 낮다면, 기기에 맞추어 와트시를 낮추어 반환하는 역할
+}
+
+typealias WattPerHour = Int
+typealias Watt = Int
