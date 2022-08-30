340be80e96012b84f215d829d55c810561b2d090
minsson <mskaang@gmail.com>
2022-08-31 02:35:56 +0900

    CoreLocation: 프로젝트 생성

---
+++ 220831-Core-Location/220831-Core-Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  220831-Core-Location
+//
+//  Created by minsson on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
