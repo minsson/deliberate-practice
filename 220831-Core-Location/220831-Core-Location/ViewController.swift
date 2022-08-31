c92c4ebca7335ec4a0b3d405e1d9571587df89ec
minsson <mskaang@gmail.com>
2022-08-31 16:10:05 +0900

    CoreLocation: 기본 구현 완료
    - info.plist에서 위치 정보 접근 승인을 위한 메시지 지정
    - CLLocationManager Delegate를 이용하여 구현
        - 델리게이트 설정
        - 원하는 위치 정확도 설정
        - info.plist에서 설정한 메시지 양식으로 유저에게 위치 정보 접근 승인 요청
        - 승인하면 위치 정보 가져오기 시작(startUpdatingLocation)
        - 시작되면 나머지는 Delegate를 통해 원하는 기능을 끼워넣어주면 됨

--- 220831-Core-Location/220831-Core-Location/ViewController.swift
+++ 220831-Core-Location/220831-Core-Location/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupLocationManager()
     }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self // 델리게이트 설정
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 우리가 원하는 위치 정확도
+        locationManager.requestWhenInUseAuthorization() // 위치정보 요청
+        // Privacy - Location When In Use Usage Description (메시지는 plist에서 설정해준 받아옴)
+        // 허용하면 유저가 설정의 프라이버시에서 해당 앱의 location services를 허용하는 것과 같아짐
 
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            print("위치 가져오기 시작함")
+        } else {
+            print("위치 서비스 허용 off")
+        }
+    }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    // 위치를 가져오면 실행됨
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations) // 위도 경도, 풍속, 날짜, 시간 등 정보가 들어있는 Array
+        
+        if let location = locations.first {
+            print("위치 업데이트!")
+            print("위도 : \(location.coordinate.latitude)")
+            print("경도 : \(location.coordinate.longitude)")
+        }
+    }
+        
+    // 위치 가져오는 과정에서 오류가 있었으면 실행됨
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error")
+    }
 }
 
